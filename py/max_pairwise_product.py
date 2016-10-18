5644f790f33115532c089d7aa81218aecc12e648
m-schrepel <mschrepel@gmail.com>
2016-10-17 21:41:19 -0700

    added python tests

--- py/max_pairwise_product.py
+++ py/max_pairwise_product.py
@@ -1,13 +1,26 @@
 # Uses python3
+from math import pow
 n = int(input())
 a = [int(x) for x in input().split()]
 assert(len(a) == n)
 
-result = 0
+def slow (a):
+    result = 0
 
-for i in range(0, n):
-    for j in range(i+1, n):
-        if a[i]*a[j] > result:
-            result = a[i]*a[j]
+    for i in range(0, n):
+        for j in range(i+1, n):
+            if a[i]*a[j] > result:
+                result = a[i]*a[j]
+    print(result)
 
-print(result)
+def fast (a):
+    b = a.sort(reverse=True)
+    print(a[0] * a[1]);
+
+def generateArray():
+    length = randint(2, 2 * math.pow(10, 5))
+    array = []
+    for i in range(0, length):
+        array.append(randInt(0, math.pow(10, 5)))
+
+assert(slow(a) == fast(a))
